3375cbd2d7607f4ce0ec349591384b973c4e723b
ismael garcia <leamsigc@hotmail.com>
2017-10-12 16:39:00 -0400

    remove class if click aout of the menu links and if add scroll event to add clas to the nav fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 //   for the nav mobile 
-var menu= document.getElementById("toggle_js");
+//nav bar
+const nav = document.querySelector('.nav'); 
+const ul= document.getElementById('menu_ul');
+const menu= document.getElementById("toggle_js");
 menu.addEventListener('click',function (e) {
     e.preventDefault();
-    var ul= document.getElementById('menu_ul');
     ul.classList.toggle('open');
+    nav.classList.add('fixed');
+});
+window.onclick = function(e){
+    if(e.target.className === 'nav__link' || e.target.className == 'toggle'){
+        if(e.target.className === 'nav__link'){
+            ul.classList.remove('open');
+        }
+    }else{
+        ul.classList.remove('open');
+    }
+}
+window.addEventListener('scroll', function(){
+    let x = window.scrollY;
+    if(x > 100 ){
+       nav.classList.add('fixed');
+    }
+    else{
+        nav.classList.remove('fixed');
+    }
 });
