b0002801b3b2a37fb719196c8193594e9b53dd44
ismael garcia <leamsigc@hotmail.com>
2017-10-12 08:54:34 -0400

    prevent default  to the link for the toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //   for the nav mobile 
 var menu= document.getElementById("toggle_js");
-menu.addEventListener('click',function () {
+menu.addEventListener('click',function (e) {
+    e.preventDefault();
     var ul= document.getElementById('menu_ul');
     ul.classList.toggle('open');
 });
