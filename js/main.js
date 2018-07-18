d7e5b32700788a5803aad2d6ab98e92e72583a8c
leamsigc <leamsigc@hotmail.com>
2018-07-18 13:36:44 -0400

    add javascript for the smooth scrollign

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ window.addEventListener('scroll', function(){
     if(x > 100 ){
         navWrapper.classList.add('fixed');
        console.log(navWrapper.style.backgroundColor);
-       navWrapper.style.backgroundColor='rgba(0,0,0,0.4)';
+       navWrapper.style.backgroundColor='rgb(49, 49, 78)';
 
     }
     else{
@@ -31,3 +31,19 @@ window.addEventListener('scroll', function(){
         
     }
 });
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    console.log('hello from the console');
+    const $nav = document.querySelector('ul[data-js="nav-bar"]');
+     $nav.addEventListener('click', scrollToElement);
+});
+
+function scrollToElement(e) {
+    e.preventDefault();
+    const elementToScrollTo = document.querySelector(`${e.target.getAttribute('href')}`);
+    elementToScrollTo.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+}
